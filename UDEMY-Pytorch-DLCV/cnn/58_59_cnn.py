c3f2d26b58d80da97b6c4fef96d07ec1abed2ab5
Kausthub Krishnamurthy <kausthub.krishnamurthy@missionsystems.com.au>
2020-09-14 17:45:38 +1000

    CNN implemented - renamed MyModel as MyMLP which makes more sense

--- UDEMY-Pytorch-DLCV/cnn/58_59_cnn.py
+++ UDEMY-Pytorch-DLCV/cnn/58_59_cnn.py
@@ -1,13 +1,17 @@
 from datetime import datetime as dt
 import torch
 import torch.nn as nn
-from torch.nn.functional import relu
+from torch.nn.functional import relu, max_pool2d
 from torch.utils.data.dataloader import DataLoader
 from torch.optim import Adam
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 from torchvision import datasets, transforms
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print("Training on device: ")
+print(device)
 
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5,), (0.5,))  # ((mean,), (std,)) --> ensuring 1 channel output. for 3 channel use ((m,m,m), (s,s,s)
@@ -38,22 +42,32 @@ for idx in np.arange(20):
 plt.show()
 
 
-class MyMLP(nn.Module):
-    def __init__(self, input_size, hidden_layer_1, hidden_layer_2, n_classes):
+class MyCNN(nn.Module):
+    def __init__(self, n_classes):
         super().__init__()
-        output_size = n_classes
-        self.linear_1 = nn.Linear(input_size, hidden_layer_1)
-        self.linear_2 = nn.Linear(hidden_layer_1, hidden_layer_2)
-        self.linear_3 = nn.Linear(hidden_layer_2, output_size)
+        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=20, kernel_size=5, stride=1)
+        self.conv_2 = nn.Conv2d(in_channels=20, out_channels=50, kernel_size=5, stride=1)
+        self.fc_1 = nn.Linear(in_features=4*4*50, out_features=500)
+        self.dropout_1 = nn.Dropout(0.5)  # adding dropout layers can be one strategy of overcoming overfitting
+        self.fc_2 = nn.Linear(in_features=500, out_features=n_classes)
 
     def forward(self, x):
-        x = relu(self.linear_1(x))
-        x = relu(self.linear_2(x))
-        x = self.linear_3(x)  # no relu since we're using cross entropy loss
-        return x
+        out_conv1 = relu(self.conv_1(x))
+        out_conv1 = max_pool2d(out_conv1, 2, 2)
+
+        out_conv2 = relu(self.conv_2(out_conv1))
+        out_conv2 = max_pool2d(out_conv2, 2, 2)
+
+        flatten = out_conv2.view(-1, 4*4*50)
+
+        out_fc1 = relu(self.fc_1(flatten))
+        out_dropout1 = self.dropout_1(out_fc1)
+
+        out_fc2 = self.fc_2(out_dropout1)  # no activation function since we're using cross-entropy loss
+        return out_fc2
 
 
-model = MyMLP(input_size=(1*28*28), hidden_layer_1=125, hidden_layer_2=65, n_classes=10)
+model = MyCNN(n_classes=10).to(device=device)
 print(model)
 
 
@@ -73,7 +87,8 @@ for e in range(epochs):
     running_corrects = 0.0
     print('')
     for images, labels in tqdm(training_loader, desc='Training Epoch {}'.format(e+1)):
-        inputs = images.view(images.shape[0], -1)  # flattens colour channel in index 0 is left alone
+        inputs = images.to(device)  # set this to device
+        labels = labels.to(device)  # set this to device
 
         outputs = model(inputs)  # infer
         loss = criterion(outputs, labels)  # calculate loss
