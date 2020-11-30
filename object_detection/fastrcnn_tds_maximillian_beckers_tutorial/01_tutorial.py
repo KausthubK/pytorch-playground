535c5174f85e2504ffb9145db853312b633d4f34
Kausthub Krishnamurthy (Mission Systems) <kausthub.krishnamurthy@missionsystems.com.au>
2020-11-30 14:24:22 +1100

    loading from full model

--- object_detection/fastrcnn_tds_maximillian_beckers_tutorial/01_tutorial.py
+++ object_detection/fastrcnn_tds_maximillian_beckers_tutorial/01_tutorial.py
@@ -147,14 +147,16 @@ for epoch in range(num_epochs):
     lr_scheduler.step()
     evaluate(model, data_loader_test, device=device)
 
-torch.save(model.state_dict(), "/home/kausthubk/out/object_detection/raccoon/model")
+torch.save(model.state_dict(), "/home/kausthubk/out/object_detection/raccoon/model_params.pt")
+torch.save(model, "/home/kausthubk/out/object_detection/raccoon/model.pt")
 
 
+print("\n\nINFERRING FROM MODEL STATE DICT\n\n")
 """
-Infer
+Infer from model_state_dict
 """
 loaded_model = get_model(num_classes = 2)
-loaded_model.load_state_dict(torch.load( "/home/kausthubk/out/object_detection/raccoon/model"))
+loaded_model.load_state_dict(torch.load( "/home/kausthubk/out/object_detection/raccoon/model_params.pt"))
 
 idx = 0
 img, _ = dataset_test[idx]
@@ -162,6 +164,41 @@ label_boxes = np.array(dataset_test[idx][1]["boxes"])
 loaded_model.eval()
 with torch.no_grad():
     prediction = loaded_model([img])
+print("\nPrediction: " + str(prediction))
+print("\nPrediction Type: " + str(type(prediction)))
+image = Image.fromarray(img.mul(255).permute(1, 2,0).byte().numpy())
+draw = ImageDraw.Draw(image)
+for elem in range(len(label_boxes)):
+    draw.rectangle([(label_boxes[elem][0], label_boxes[elem][1]), (label_boxes[elem][2], label_boxes[elem][3])],
+                   outline="green", width=3)
+
+for element in range(len(prediction[0]["boxes"])):
+    boxes = prediction[0]["boxes"][element].cpu().numpy()
+    score = np.round(prediction[0]["scores"][element].cpu().numpy(), decimals=4)
+
+    if score > 0.8:
+        draw.rectangle([(boxes[0], boxes[1]), (boxes[2], boxes[3])], outline ="red", width =3)
+        draw.text((boxes[0], boxes[1]), text = str(score))
+
+image.show()
+
+
+"""
+Infer from full model
+"""
+device = torch.device("cuda")
+print("\n\nINFERRING FROM FULL MODEL\n\n")
+loaded_full_model = torch.load( "/home/kausthubk/out/object_detection/raccoon/model.pt", map_location="cuda:0")
+loaded_full_model.to(device)
+
+idx = 0
+img, _ = dataset_test[idx]
+label_boxes = np.array(dataset_test[idx][1]["boxes"])
+loaded_full_model.eval()
+with torch.no_grad():
+    prediction = loaded_full_model([img.to(device)])
+print("\nPrediction: " + str(prediction))
+print("\nPrediction Type: " + str(type(prediction)))
 image = Image.fromarray(img.mul(255).permute(1, 2,0).byte().numpy())
 draw = ImageDraw.Draw(image)
 for elem in range(len(label_boxes)):
